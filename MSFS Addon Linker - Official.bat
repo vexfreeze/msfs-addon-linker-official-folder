836f4245b064389f911e9a451565e51b6b4e73c7
vexfreeze <52785190+vexfreeze@users.noreply.github.com>
2023-04-24 18:28:42 +0200

    Update MSFS Addon Linker - Official.bat

--- MSFS Addon Linker - Official.bat
+++ MSFS Addon Linker - Official.bat
@@ -1,5 +1,5 @@
 rmdir "C:\ProgramData\MSFS Addons Linker"
 mklink /D "C:\ProgramData\MSFS Addons Linker" "C:\ProgramData\MSFS Addons Linker - Official"
-cd c:\Users\weksi\Documents\MSFS_Addons_Linker
+cd c:\Users\username\Documents\MSFS_Addons_Linker
 .\MSFS_AddonsLinker.exe
 #pause
