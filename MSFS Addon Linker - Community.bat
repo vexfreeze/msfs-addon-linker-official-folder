6549e35260b06ba443d2e2854958c14174637afc
vexfreeze <52785190+vexfreeze@users.noreply.github.com>
2023-10-22 19:57:59 +0200

    Update MSFS Addon Linker - Community.bat

--- MSFS Addon Linker - Community.bat
+++ MSFS Addon Linker - Community.bat
@@ -1,5 +1,16 @@
+@echo off
+tasklist /fi "ImageName eq MSFS_AddonsLinker.exe" /fo csv 2>NUL | find /I "MSFS_AddonsLinker.exe">NUL
+if "%ERRORLEVEL%"=="0" goto running
+
 rmdir "C:\ProgramData\MSFS Addons Linker"
 mklink /D "C:\ProgramData\MSFS Addons Linker" "C:\ProgramData\MSFS Addons Linker - Community"
-cd c:\Users\username\Documents\MSFS_Addons_Linker
+cd c:\Users\weksi\Documents\MSFS_Addons_Linker
 .\MSFS_AddonsLinker.exe
-#pause
+goto end
+
+:running
+echo MSFS Addon Linker is already running!
+pause
+goto end
+
+:end
